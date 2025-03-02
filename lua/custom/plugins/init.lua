c9f73149600d63346a4bd005b2d62b4d405a72fb
Armand-Cezar Mathe <me@cezarmathe.com>
2025-03-02 20:30:53 +0200

    Update config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,37 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'nvim-orgmode/orgmode',
+  event = 'VeryLazy',
+  ft = { 'org' },
+  config = function()
+    -- Setup orgmode
+    require('orgmode').setup {
+      org_agenda_files = '~/org/**/*',
+      org_default_notes_file = '~/org/refile.org',
+    }
+
+    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+    -- add ~org~ to ignore_install
+    -- require('nvim-treesitter.configs').setup({
+    --   ensure_installed = 'all',
+    --   ignore_install = { 'org' },
+    -- })
+  end,
+}
+--{
+--  'nvim-orgmode/telescope-orgmode.nvim',
+--  event = 'VeryLazy',
+--  dependencies = {
+--   'nvim-orgmode/orgmode',
+--   'nvim-telescope/telescope.nvim',
+--  },
+--  config = function()
+--    require('telescope').load_extension 'orgmode'
+--
+--    vim.keymap.set('n', '<leader>ro', require('telescope').extensions.orgmode.refile_heading)
+--    vim.keymap.set('n', '<leader>so', require('telescope').extensions.orgmode.search_headings)
+--    vim.keymap.set('n', '<leader>io', require('telescope').extensions.orgmode.insert_link)
+--  end,
+--}
